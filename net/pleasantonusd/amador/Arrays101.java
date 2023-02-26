1c224c0627446596aebdc7bcfc350e6b06c96d75
giuliacolaco <giulia.colaco.silva@gmail.com>
2023-02-26 15:50:46 -0800

    Arrays Examples

---
+++ net/pleasantonusd/amador/Arrays101.java
@@ -0,0 +1,14 @@
+package net.pleasantonusd.amador;
+
+public class Arrays101 {
+
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+
+}
