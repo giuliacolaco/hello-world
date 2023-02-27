63285ebef35300b17678e973820099ed8e5adf8b
giuliacolaco <giulia.colaco.silva@gmail.com>
2023-02-26 18:18:59 -0800

    Ex2

--- net/pleasantonusd/amador/ArraysEx2.java
+++ net/pleasantonusd/amador/ArraysEx2.java
@@ -5,9 +5,12 @@ public class ArraysEx2 {
 
     public static void main(String[] args) {
         double[] numbers = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10};
+        double total = 0;
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+            total += numbers[i];
         }
+        double media = total/ numbers.length;
+        System.out.println(media);
     }
 
 
