7ccca31bd24fc79c2c3a1f2f8cbabed9b4302a5c
giuliacolaco <giulia.colaco.silva@gmail.com>
2023-02-26 17:57:40 -0800

    Ex1

--- net/pleasantonusd/amador/ArraysEx1.java
+++ net/pleasantonusd/amador/ArraysEx1.java
@@ -4,8 +4,8 @@ public class ArraysEx1 {
 
 
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-        for (int i = 0; i < numbers.length; i++) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = numbers.length-1; i >= 0; i--) {
             System.out.println(numbers[i]);
         }
     }
--- net/pleasantonusd/amador/ArraysEx2.java
+++ net/pleasantonusd/amador/ArraysEx2.java
@@ -1,10 +1,10 @@
 package net.pleasantonusd.amador;
 
-public class Arrays101 {
+public class ArraysEx2 {
 
 
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        double[] numbers = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10};
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
